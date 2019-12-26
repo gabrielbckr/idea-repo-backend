7c38210c5549fb8ced5dcce56a5a119d1388b3d3
gabriel <gabriel.becker.59@gmail.com>
2019-12-26 19:01:02 -0300

    Add contact field

--- Domain/Idea.py
+++ Domain/Idea.py
@@ -2,13 +2,15 @@ from marshmallow import Schema, fields
 
 
 class Idea():
-    def __init__(self, title="", description="", wholeIdea=""):
+    def __init__(self, title="", contact="", description="", wholeIdea=""):
         self.title = title
         self.description = description
         self.wholeIdea = wholeIdea
+        self.contact = contact
 
 
 class IdeaSchema(Schema):
     title = fields.Str()
     description = fields.Str()
     wholeIdea = fields.Str()
+    contact = fields.Str()
