5f3740272a1bfe3ab430f7620394468444015d20
gabriel <gabriel.becker.59@gmail.com>
2019-12-26 19:01:58 -0300

    Ignore cors allowing all endpoints

--- idea-repo-server.py
+++ idea-repo-server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response, jsonify, json
 from Business import IdeaBusiness
 
 app = Flask(__name__)
@@ -8,16 +8,22 @@ ideaBusiness = IdeaBusiness.IdeaBusiness()
 
 @app.route("/get-ideas", methods=["GET"])
 def getIdeas():
-    return ideaBusiness.GetIdeas()
+    responseContent = ideaBusiness.GetIdeas()
+    return Response(responseContent, mimetype='application/json')
 
 
 @app.route("/add-idea", methods=["POST"])
 def addIdea():
-    title = request.form['title']
-    description = request.form['description']
-    wholeIdea = request.form['wholeIdea']
-    ideaBusiness.SaveIdea({title, description, wholeIdea})
+    data = request.get_json()
+    ideaBusiness.SaveIdea(data["title"], data["contact"], data["description"], data["wholeIdea"])
+    return "200"
 
+
+@app.after_request  # blueprint can also be app~~
+def after_request(response):
+    header = response.headers
+    header["Access-Control-Allow-Origin"] = '*'
+    return response
 
 
 if __name__ == '__main__':
