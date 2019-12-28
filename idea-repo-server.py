b7b1363f965e4f81075e740679d02487f4e4e6d5
gabriel <gabriel.becker.59@gmail.com>
2019-12-27 23:14:21 -0300

    Add cors lib to solve post to option redirect

--- idea-repo-server.py
+++ idea-repo-server.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, Response, jsonify, json
+from flask import Flask, request, Response
+from flask_cors import CORS
+
 from Business import IdeaBusiness
 
 app = Flask(__name__)
-
+CORS(app)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 ideaBusiness = IdeaBusiness.IdeaBusiness()
 
 
@@ -17,13 +20,6 @@ def addIdea():
     data = request.get_json()
     ideaBusiness.SaveIdea(data["title"], data["contact"], data["description"], data["wholeIdea"])
     return "200"
-
-
-@app.after_request  # blueprint can also be app~~
-def after_request(response):
-    header = response.headers
-    header["Access-Control-Allow-Origin"] = '*'
-    return response
 
 
 if __name__ == '__main__':
